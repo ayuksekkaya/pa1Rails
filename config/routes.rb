542393d69b75c4adb065e998e101f78329afd945
aliYuksekkayaBrandeis <ayuksekkaya@brandeis.edu>
2021-03-20 14:43:56 -0400

    finished sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,15 @@ Rails.application.routes.draw do
   resources :subjects
   resources :courses
   resources :instructors
+  resources :users
+
+  get '/courses', to: 'courses#show'
+  get '/subjects', to: 'subjects#show'
+  get '/instructors', to: 'instructors#show'
+
+  get '/signup', to: 'users#new'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root "welcome#index"
